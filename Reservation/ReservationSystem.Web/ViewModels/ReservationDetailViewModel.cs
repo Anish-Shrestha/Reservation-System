14242798e0597afbe3cba13fc2f1d04c4b622f2f
Anish Man Shrestha <anish.shrestha237@gmail.com>
2016-10-20 12:56:47 -0700

    viewmodel added

--- Reservation/ReservationSystem.Web/ViewModels/ReservationDetailViewModel.cs
+++ Reservation/ReservationSystem.Web/ViewModels/ReservationDetailViewModel.cs
@@ -7,5 +7,16 @@ namespace ReservationSystem.Web.ViewModels
 {
     public class ReservationDetailViewModel
     {
+        public int Id { get; set; }
+        public int ReservationId { get; set; }
+        public int Adults { get; set; }
+        public int Childrens { get; set; }
+        public DateTime? DateCreated { get; set; }
+        public DateTime? DateUpdated { get; set; }
+
+        public ReservationDetailViewModel()
+        {
+            DateCreated = DateTime.Now;
+        }
     }
 }
